01de08dfb0e00708f0df491e2fc6864e1352bd2d
hujiohujio <hujio_hujio0100@yahoo.co.jp>
2020-12-08 03:31:23 +0900

    ユーザー情報の編集ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :profile, :fav_subj, :weak_subj)
+  end
+
+
 end
